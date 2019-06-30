ea1f43ab2e27ce90e8748f71f1fe1b43f2da678c
王彦睿 <tieunit@gmail.com>
2019-06-30 22:52:32 +0800

    删除无用内容

--- App.js
+++ App.js
@@ -1,122 +1,70 @@
 import React from "react";
-import { View, Text, Button, SectionList, StyleSheet } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import {View, Text, Button, SectionList, StyleSheet} from "react-native";
+import {createStackNavigator, createAppContainer} from "react-navigation";
 import DetailsScreen from "./source/DetailScreen"
-import Row from "./source/Row"
-import RandomContact from '/source/RandomContact'
+import ContactView from './source/ContactView'
 
-// import console = require("console");
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
+    container: {
+        flex: 1,
+        marginTop: 20,
+    },
 });
 
 
 const RowModel = (props) => ({
-  key: props.index + "" + false,
-  index: props.index,
-  title: props.title,
-  checked: props.checked,
-  onDelete: props.onDelete,
+    key: props.index + "" + false,
+    index: props.index,
+    title: props.title,
+    checked: props.checked,
+    onDelete: props.onDelete,
 })
 
 class HomeScreen extends React.Component {
 
 
+    componentDidMount() {
 
-  componentDidMount() {
-    const pp = RandomContact
-  }
-
-
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      model: []
-    }
-  }
-
-  /**
-    _generateHeader(original) {
-        return <Text>{original.title}</Text>
     }
 
-  _deleteItem(index) {
-    const newList = this.state.model.filter((singleModel) => singleModel.index !== index);
-    this.setState({
-      model: newList,
-    })
-  }
 
-  _addItem() {
-    let todo = new RowModel({title: this.state.count + 1 + "", index: this.state.count + 1, checked: false});
-    this.setState(preState => ({
-      count: preState.count + 1,
-      model: [...preState.model, todo],
-    }))
-    this.state.model.reduce((previousValue, nextValue) => {}, {})
-  }
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+            model: []
+        }
+    }
 
-  _toggleItem(index) {
-    let itemToToggle = this.state.model.filter((singleItem) => singleItem.index === index)[0];
-    let arrayIndex = this.state.model.indexOf(itemToToggle);
-    itemToToggle.checked = !itemToToggle.checked
-    
-    this.setState(preState => ({model: [...preState.model]}))
-  }
+    render() {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
 
-  _generateItem(original) {
-    return (
-      <Row 
-        title={original.title}
-        isOn={original.checked}
-        onDelete={() => {this._deleteItem(original.index)}}
-        onToggle={() => {this._toggleItem(original.index)}}
-      />
-    )
-  }
-   */
+                <Button
+                    title="Go to Contacts"
+                    onPress={() => this.props.navigation.navigate('Contacts')}
+                />
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-        <Button 
-          title="add a todo"
-          onPress={() => this._addItem()}
-        />
-        /**
-        <SectionList
-          style={styles.container}
-          renderItem={({item}) => this._generateItem(item)}
-          renderSectionHeader={({section}) => this._generateHeader(section)}
-          sections={[{
-            title: 'A',
-            data: this.state.model,
-          }]}
-        />
-        **/
-      </View>
-    );
-  }
+            </View>
+        );
+    }
 }
 
 const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen
-  },
-  {
-    initialRouteName: "Home"
-  }
+    {
+        Home: HomeScreen,
+        Details: DetailsScreen,
+        Contacts: ContactView,
+    },
+    {
+        initialRouteName: "Home"
+    }
 );
 
 export default createAppContainer(AppNavigator);
---
+++ source/ContactView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+class ContactView extends React.Component {
+    render() {
+        return (
+            <Text>123</Text>
+        );
+    }
+}
+
+export default ContactView
