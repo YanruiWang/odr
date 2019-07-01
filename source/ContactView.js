92363e9b977af664b3ba52dbee75d79a4a4e98c7
王彦睿 <tieunit@gmail.com>
2019-07-01 22:43:03 +0800

    可以显示 section list 了

--- source/ContactView.js
+++ source/ContactView.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { View, Text, Button, SectionList } from "react-native";
+import {Button, View, SectionList, Text} from "react-native";
 import {fake} from './RandomContact';
 
 
 const acc = (previous, current) => {
-    const inintial = current.name[0]
-    if (previous[inintial]) {
-        previous[inintial].push(current)
+    const initial = current.name[0]
+    if (previous[initial]) {
+        previous[initial].push(current)
     } else {
-        previous[inintial] = [current]
+        previous[initial] = [current]
     }
     return previous
+};
+
+class ContactHeader extends React.Component {
+    render() {
+        return(<Text>{this.props.name}</Text>)
+    }
+}
+
+class SingleContact extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.name}</Text>
+                <Text>{this.props.phone}</Text>
+            </View>
+        )
+    }
 }
 
 class ContactView extends React.Component {
 
+    _generateData = () => {
+        const fakeContacts = this._sortContact(this.state.originalContacts)
+        const fakeKeys = Object.keys(fakeContacts)
+        const realData = fakeKeys.map((singleKey) => ({data: fakeContacts[singleKey], title: singleKey}))
+        this.setState({realData: realData})
+    }
+
     _sortContact = (previousContact) => {
-        const sorted = previousContact.reduce(acc, {})
-        return sorted
+        return previousContact.reduce(acc, {})
+    }
+
+    componentDidMount(): void {
+        this._generateData()
     }
 
     constructor() {
@@ -25,18 +52,27 @@ class ContactView extends React.Component {
         this.state = {
             contacts: {},
             originalContacts: fake,
+            realData: [],
         }
     }
 
+    _renderItem = ({item}) => <SingleContact name={item.name} phone={item.phone}/>
+
+    _renderSection = ({section}) => <ContactHeader name={section.title}/>
+
     _sort = () => {
-        const fakeContacts = this._sortContact(this.state.originalContacts)
-        this.setState({contacts: fakeContacts})
+        this._generateData()
     }
 
     render() {
         return (
             <View>
-                <Button onPress={this._sort} title="sort"></Button>
+                <SectionList
+                    renderItem={this._renderItem}
+                    renderSectionHeader={this._renderSection}
+                    sections={this.state.realData}
+                    keyExtractor={(item, index) => item + index}
+                />
             </View>
             
         );
--- source/RandomContact.js
+++ source/RandomContact.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const rand = (max, min = 0) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const firstNames = ["Jordan", "Richard", "John", "Mike"];
+const firstNames = ["Jordan", "Richard", "John", "Mike", "Fuck", "Sexy", "Sherlock", "Tim", "Martin"];
 
-const lastNames = ["Trump", "Lee", "Watanabe", "Bush", "White", "Black"];
+const lastNames = ["Trump", "Lee", "Cook", "Bush", "White", "Black", "Freeman"];
 
 const generateName = () => `${firstNames[rand(firstNames.length - 1)]} ${lastNames[rand(lastNames.length - 1)]}`;
 
@@ -12,6 +12,6 @@ const generatePhoneNumber = () => `${rand(999,100)}-${rand(999,100)}-${rand(9999
 
 const generateContact = () => ({name: generateName(), phone: generatePhoneNumber()});
 
-const fake = Array.from({length: 100}, generateContact);
+const fake = Array.from({length: 200}, generateContact);
 
 export {fake};
